0c560339b0d5575e7837226976a57be365a68d69
PAL Durgesh <durgesh.pal@soprasteria.com>
2020-05-28 00:10:53 +0530

    components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sms-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
